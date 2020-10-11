aa5842b6e0a1b50ac07521bfadbf4b68932c7d9b
Utkarsh Gupta <utkarsh@debian.org>
2020-10-11 21:33:59 +0530

    Enable error checking

--- change-syllabus/syllabus_editing.sh
+++ change-syllabus/syllabus_editing.sh
@@ -8,5 +8,21 @@ docxFile=$1
 # to convert the given .docx file to the .txt file for easy text manipulation.
 docx2txt $docxFile SyllabusLinuxForDevices.txt
 
+# error checking
+if [[ $? != 0  ]]
+then
+  echo "Error has occured. Couldn't convert the $1 to a text file."
+  exit 1
+fi
+
 # changing the phrase "Shell script" to "Write a shell script" using sed.
 sed -i 's/Shell script/Write a shell script/g' SyllabusLinuxForDevices.txt
+
+# tell the user that the editing has been done.
+if [[ $? -eq 0  ]]
+then
+  echo "Editing done. You can see the newly created SyllabusLinuxForDevices.txt file."
+else
+  echo "Error has occured. Couldn't edit $1 file."
+  exit 1
+fi
