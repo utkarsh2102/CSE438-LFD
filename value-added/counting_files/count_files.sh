d03dc1c58a49da8bf9b2dbdbe29fd3e1b1489e31
Utkarsh Gupta <utkarsh@debian.org>
2020-10-13 23:18:23 +0530

    Add a minimal counting files script

---
+++ value-added/counting_files/count_files.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+# Written by Utkarsh Gupta <utkarsh@debian.org>
+
+# Intructions to run:
+# Run this script with a path to directory as a
+# command-line argument.
+# ./count_files.sh $path_to_some_directory
+#
+# For example:
+# $ ./count_files.sh /etc/grub.d
+
+if [ -d "$1" ]
+then
+  ls $1 | wc -l
+else
+  echo "No such directory named $1"
+fi
